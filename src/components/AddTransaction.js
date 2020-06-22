32f433d5dbb9e57b71c909b0fd4d4ab201b9fbc7
Karen Zhao <karen.zhao18@gmail.com>
2020-06-22 11:34:45 -0400

    add action

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,13 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState('');
 
+    const { addTransaction } = useContext(GlobalContext);
+
+    // amount will be a string, convert into number using + or Math.floor
+    const onSubmit = (e) => { 
+        e.preventDefault(); 
+        
+        const newTransaction = { 
+            id: Math.floor(Math.random() * 100000000), 
+            text, 
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+    }
+
     return (
     <>
         <h3>Add new transaction</h3>
-        <form>
+        <form onSubmit={onSubmit}>
 
             <div className="form-control">
             <label htmlFor="text">Text</label>
