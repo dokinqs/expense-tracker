a4c51bc0d27ae5330b581d13a4b9a7ba9caec351
Karen Zhao <karen.zhao18@gmail.com>
2020-06-22 23:26:01 -0400

    remove hardcoded initialstate

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -2,12 +2,7 @@ import React, { createContext, useReducer } from 'react';
 import AppReducer from './AppReducer';
 
 const initialState = {
-    transactions: [
-        { id: 1, text: 'Pad Thai', amount: -14 },
-        { id: 2, text: 'Hustlin', amount: 80 },
-        { id: 3, text: 'Oh no', amount: -50 },
-        { id: 4, text: 'Yay', amount: 30 }
-    ]
+    transactions: []
 }
 
 // Create Context
